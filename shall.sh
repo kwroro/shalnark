ff859683442482dbd80484303c0fe57fbde397ba
kwroro <74576831+kwroro@users.noreply.github.com>
2020-11-17 06:55:16 +0300

    Create shall.sh

---
+++ shall.sh
@@ -0,0 +1,429 @@
+#!/system/xbin/bash
+
+#z0lldyck
+
+#recodelu_memang_nub
+
+blue='\033[34;1m'
+
+green='\033[32;1m'
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'
+
+yellow='\033[33;1m'
+
+sleep 1
+
+figlet Z0LLDYCK | lolcat
+
+figlet Mix Tools | lolcat
+
+echo -e " Welcome To Mix Tools Facebook" | lolcat
+
+echo -e " telegram  : z0lldyck" | lolcat
+
+echo -e " Youtube   : z0lldyck" | lolcat
+
+echo -e " Instagram : z0lldyck" | lolcat
+
+echo -e " ∆ Silahkan Pilih Tools Yang Ingin Kalian Install ∆" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "1.Tools Facebook cracker" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "2.Tools FB Mail Cracker" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "3.Tools Inshackle (Autofollowers etc)" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "4.Tools Instagram Brute Force (Tested Work 100%)" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "5.Tools Instagram (Followers,Download Stories etc)" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "6.Tools Instagram Followers Scraper" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "7.Tools Auto Report Facebook" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "8.Tools Private Tools Instagram v2.5" | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "9.Tools Instagram Bot v2 " | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "10.Tools Instagram Bot v3 " | lolcat
+
+echo -e "__________________________________" | lolcat
+
+echo -e "11.Tools Open Source Instagram (OSI)" | lolcat
+
+echo ""
+
+echo -e "0.Keluar" | lolcat
+
+read -p "kakroboto | " asu
+
+if [ $asu = 1 ] || [ $asu = 1 ]
+
+then
+
+clear
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+pkg update
+
+pkg upgrade
+
+pkg install git
+
+pkg install python2
+
+git clone https://github.com/V4N654T/fb-hacker
+
+cd fb-hacker
+
+python2 fb.py
+
+fi
+
+if [ $asu = 2 ] || [ $asu = 2 ]
+
+then
+
+clear
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+pkg update && pkg upgrade
+
+pkg install python
+
+pkg install python2
+
+pkg install git 
+
+pkg install php
+
+pkg install openssh
+
+git clone https://github.com/botolmehedi/bvai
+
+pip2 install requests
+
+pip2 install mechanize
+
+ls
+
+cd bvai
+
+python2 bvai.py
+
+fi
+
+if [ $asu = 3 ] || [ $asu = 3 ]
+
+then
+
+clear
+
+figlet kakroboto | lolcat
+
+echo -e "Auto Install" | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+apt install git
+
+apt install bash
+
+git clone https://github.com/xd20111/inshackle
+
+mv inshackle $HOME
+
+cd $HOME/inshackle
+
+bash inshackle.sh
+
+fi
+
+if [ $asu = 4 ] || [ $asu = 4 ]
+
+then
+
+clear
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+apt install git
+
+apt install python
+
+git clone https://github.com/Pure-L0G1C/Instagram
+
+mv Instagram $HOME
+
+cd $HOME/Instagram
+
+pip install -r requirements.txt
+
+pip install bs4
+
+python instagram.py
+
+fi
+
+if [ $asu = 5 ] || [ $asu = 5 ]
+
+then
+
+clear
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+apt install git
+
+git clone https://github.com/rixon-cochi/Lucifer.git
+
+mv Lucifer $HOME
+
+cd $HOME/Lucifer
+
+bash setup.sh
+
+bash instacracker.sh
+
+fi
+
+if [ $asu = 6 ] || [ $asu = 6 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt install git
+
+git clone  https://github.com/nOOBIE-nOOBIE/Instagram-Follower-Scraper
+
+mv Instagram-Follower-Scraper $HOME
+
+cd $HOME/Instagram-Follower-Scraper
+
+pip install -r requirements.txt
+
+pip install setuptools
+
+pip install instaloader
+
+python instabot.py
+
+fi
+
+if [ $asu = 7 ] || [ $asu = 7 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+git clone https://github.com/IlayTamvan/Report
+
+cd Report
+
+unzip Report.zip
+
+python2 Report.py
+
+fi
+
+if [ $asu = 8 ] || [ $asu = 8 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+apt install unzip -y
+
+pkg install nodejs-lts -y
+
+git clone https://github.com/officialputuid/toolsig.git
+
+mv toolsig $HOME
+
+cd $HOME/toolsig
+
+unzip lib.zip
+
+node index.js
+
+fi
+
+if [ $asu = 9 ] || [ $asu = 9 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+apt install python
+
+apt install git
+
+git clone https://github.com/N74NK/taig
+
+mv taig $HOME
+
+cd $HOME/taig
+
+pip install -r requirements.txt
+
+python taig.py
+
+fi
+
+if [ $asu = 10 ] || [ $asu = 10 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+apt update
+
+apt upgrade
+
+pkg install nodejs-lts -y
+
+git clone https://github.com/kumpulanremaja/ig
+
+mv ig $HOME
+
+cd $HOME/ig
+
+unzip node_modules.zip
+
+npm install
+
+node index.js
+
+fi
+
+if [ $asu = 11 ] || [ $asu = 11 ]
+
+then
+
+figlet kakroboto | lolcat
+
+echo "Untuk Tutorial Silahkan Comment Atau Join Grup"
+
+sleep 3
+
+git clone https://github.com/th3unkn0n/osi.ig.git
+
+mv osi.ig $HOME
+
+cd $HOME/osi.ig
+
+chmod +x install.sh
+
+bash install.sh
+
+python main.py -u
+
+fi
+
+if [ $asu = 0 ] || [ $asu = 0 ]
+
+then
+
+echo -e "z0lldyck" | lolcat
+
+echo -e "No System Is Safe" | lolcat
+
+echo -e "Be Carrefuly On Internet" | lolcat
+
+echo -e "Malware Everywhere" | lolcat
+
+echo -e "Thanks For Use This Tools" | lolcat
+
+xdg-open https://www.youtube.com/channel/UCwmJg-hAMnu2mK_5qJ5sv7w
+
+exit
+
+fi
