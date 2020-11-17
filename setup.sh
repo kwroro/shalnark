4c068fed7f62f954f5ec69c140e69372e224b269
kwroro <74576831+kwroro@users.noreply.github.com>
2020-11-17 06:55:48 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/system/xbin/bash
+
+apt install git -y
+
+apt install bash -y
+
+apt install python -y
+
+apt install figlet -y
+
+apt install ruby -y
+
+gem install lolcat
+
+apt install toilet -y
+
+apt install python2 -y
+
+apt install cowsay -y
+
+pip2 install requests
+
+pip2 install mechanize
+
+pip install requests
+
+pip install mechanize
+
+xdg-open https://www.youtube.com/channel/UCwmJg-hAMnu2mK_5qJ5sv7w
